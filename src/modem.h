039007c2624ea40514495c9c839641bbc72e82d7
Stephan Bischoff <stephan.bischoff@digle.net>
2016-10-27 18:27:02 +0200

    incorporated feature/receive into develop

--- src/modem.h
+++ src/modem.h
@@ -8,18 +8,18 @@ protected:
     unsigned int frequency_;
     unsigned int samples_;
     unsigned int baud_;
-    
+
 public:
-    
+
     Modem(unsigned int frequency, unsigned int samples, unsigned int baud) :
         frequency_(frequency),
         samples_(samples),
         baud_(baud) {};
-    
+
     const unsigned int getFrequency() const {return frequency_;}
     const unsigned int getSamples() const {return samples_;}
     const unsigned int getBaud() const {return baud_;}
-        
-    virtual std::vector<float> modulate(Dataframe frame) const = 0;
-    virtual Dataframe demodulate(std::vector<float> signal) const = 0;
+
+    virtual const std::vector<float> modulate(Dataframe frame) const = 0;
+    virtual const Dataframe demodulate(std::vector<float>& signal) const = 0;
 };
